81085301dd0afb8e5fe5ba44c41ed92ed72e1dd2
Ziya SARIKAYA <sarikayaziya@gmail.com>
2015-03-19 10:41:33 +0200

    Increment package version

--- Httwrap/Properties/AssemblyInfo.cs
+++ Httwrap/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.5")]
-[assembly: AssemblyFileVersion("1.0.5")]
+[assembly: AssemblyVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.0.6")]
 [assembly: InternalsVisibleTo("Httwrap.Tests")]
