deb68f0c42ac448db8928173776d091d25473aba
Oguzhan Demir <oguzhandemir92@gmail.com>
2015-03-24 00:35:24 +0200

    Username, password oauth try1.

--- Httwrap/Auth/OAuthCredentials.cs
+++ Httwrap/Auth/OAuthCredentials.cs
@@ -1,11 +1,17 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Net.Http.Headers;
 
 namespace Httwrap.Auth
 {
     public class OAuthCredentials : Credentials
     {
-        private readonly string _token;
+        private string _token;
+        private readonly string _username;
+        private readonly string _password;
+        private readonly string _grantType;
+        private readonly string _requestEndpoint;
         private readonly bool _isTls;
 
         public OAuthCredentials(string token, bool isTls = false)
@@ -15,12 +21,38 @@ namespace Httwrap.Auth
             _isTls = isTls;
         }
 
+        public OAuthCredentials(string username, string password, string requestEndpoint)
+        {
+            Check.NotNullOrEmpty(username, "username");
+            Check.NotNullOrEmpty(password, "password");
+            Check.NotNullOrEmpty(requestEndpoint, "requestEndpoint");
+            _username = username;
+            _password = password;
+            _requestEndpoint = requestEndpoint;
+            _grantType = "password";
+
+        }
+
         public override HttpClient BuildHttpClient(HttpMessageHandler httpHandler = null)
         {
-            HttpClient httpClient = httpHandler != null ? new HttpClient(httpHandler) : new HttpClient();
+            var httpClient = httpHandler != null ? new HttpClient(httpHandler) : new HttpClient();
+
+            if (String.IsNullOrEmpty(_token))
+            {
+                FormUrlEncodedContent content = new FormUrlEncodedContent(new Dictionary<string, string>
+                {
+                    {"username", _username},
+                    {"password", _password},
+                    {"grant_type", _grantType}
+                });
+
+                HttpResponseMessage response = httpClient.PostAsync(_requestEndpoint, content).Result;
+                Token token = new HttwrapResponse(response).ReadAs<Token>();
+                _token = token.AccessToken;
+
+            }
 
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
-
             return httpClient;
         }
 
---
+++ Httwrap/Auth/Token.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Httwrap.Auth
+{
+    class Token
+    {
+        public Token()
+        {
+        }
+
+        [JsonProperty(PropertyName = "access_token")]
+        public string AccessToken { get; set; }
+    }
+}
