70ba9d88d5baf8b1695026dcf7dd5196c7d361d7
Ziya SARIKAYA <sarikayaziya@gmail.com>
2016-06-12 21:34:03 +0300

    fix lastet commit

--- Httwrap/HttwrapClient.cs
+++ Httwrap/HttwrapClient.cs
@@ -4,7 +4,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using Httwrap.Interface;
+using Httwrap.Interface;    
 
 namespace Httwrap
 {
--- Httwrap/Interface/IHttwrapClient.cs
+++ Httwrap/Interface/IHttwrapClient.cs
@@ -7,16 +7,28 @@ namespace Httwrap.Interface
 {
     public interface IHttwrapClient
     {
-        Task<IHttwrapResponse> GetAsync(string path, Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
-        Task<IHttwrapResponse> GetAsync(string path, object payload, Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
-        Task<IHttwrapResponse<T>> GetAsync<T>(string path, Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
+        Task<IHttwrapResponse> GetAsync(string path, Action<HttpStatusCode, string> errorHandler = null,
+            Dictionary<string, string> customHeaders = null);
+
+        Task<IHttwrapResponse> GetAsync(string path, object payload, Action<HttpStatusCode, string> errorHandler = null,
+            Dictionary<string, string> customHeaders = null);
+
+        Task<IHttwrapResponse<T>> GetAsync<T>(string path, Action<HttpStatusCode, string> errorHandler = null,
+            Dictionary<string, string> customHeaders = null);
 
         Task<IHttwrapResponse<T>> GetAsync<T>(string path, object payload,
-            Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
+            Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> customHeaders = null);
 
-        Task<IHttwrapResponse> PutAsync<T>(string path, T data, Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
-        Task<IHttwrapResponse> PostAsync<T>(string path, T data, Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
-        Task<IHttwrapResponse> DeleteAsync(string path, Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
-        Task<IHttwrapResponse> PatchAsync<T>(string path, T data, Action<HttpStatusCode, string> errorHandler = null, Dictionary<string, string> extraHeaders = null);
+        Task<IHttwrapResponse> PutAsync<T>(string path, T data, Action<HttpStatusCode, string> errorHandler = null,
+            Dictionary<string, string> customHeaders = null);
+
+        Task<IHttwrapResponse> PostAsync<T>(string path, T data, Action<HttpStatusCode, string> errorHandler = null,
+            Dictionary<string, string> customHeaders = null);
+
+        Task<IHttwrapResponse> DeleteAsync(string path, Action<HttpStatusCode, string> errorHandler = null,
+            Dictionary<string, string> customHeaders = null);
+
+        Task<IHttwrapResponse> PatchAsync<T>(string path, T data, Action<HttpStatusCode, string> errorHandler = null,
+            Dictionary<string, string> customHeaders = null);
     }
 }
